c4a2e46c4f00e5bdc2a6b9a868cdac9096579c36
dev12 <46024391+Aidann32@users.noreply.github.com>
2023-09-21 15:07:13 +0500

    Added serializers

--- footballers_api/apps/footballers/serializers.py
+++ footballers_api/apps/footballers/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 
-from .models import Footballer
+from .models import Footballer, Country, FootballClub
+
+
+class FootballClubSerializer(serializers.ModelSerializer):
+    country = serializers.StringRelatedField(many=False)
+
+    class Meta:
+        model = FootballClub
+        fields = ('country', 'name')
 
 
 class FootballerSerializer(serializers.ModelSerializer):
+    club = FootballClubSerializer()
+    
     class Meta:
         model = Footballer
-        fields = '__all__'
+        fields = ('first_name', 'last_name', 'club', 'shirt_number', 'date_of_birth')
         
